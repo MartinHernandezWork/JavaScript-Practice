51a250fa355500cef8bd69926e9c034dce0dbdab
Martin Hernandez <martinhernandezunvime@gmail.com>
2023-04-12 15:56:24 -0300

    Update 12/4

---
+++ Practica JavaScript 13.js
@@ -0,0 +1,39 @@
+// Iterar con While Loops.
+
+var myArray = [];
+
+var i = 0;
+while(i < 5) {
+	myArray.push(i);
+	i++;
+}
+
+//console.log(myArray);
+
+// Iterar con For Loops.
+
+ var ourArray = [];
+
+ for (var i = 0; i < 5; i++) {
+ 	ourArray.push(i);
+ }
+
+//console.log(ourArray);
+
+// Iterar numeros pares e impares con un For Loop.
+
+ var parArray = [];
+
+ for (var i = 0; i < 10; i += 2) {
+ 	parArray.push(i);
+ }
+
+ //console.log(parArray);
+
+ var imparArray = [];
+
+ for (var i = 1; i < 10; i += 2) {
+ 	imparArray.push(i);
+ }
+
+//console.log(imparArray);
