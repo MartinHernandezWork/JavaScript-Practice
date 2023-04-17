3bb435475500a8e13a53b4c981f0ec398035e655
Martin Hernandez <martinhernandezunvime@gmail.com>
2023-04-17 19:59:15 -0300

    Practica dia 17/04
    For Loops

--- Practica JavaScript 13.js
+++ Practica JavaScript 13.js
@@ -37,3 +37,54 @@ while(i < 5) {
  }
 
 //console.log(imparArray);
+
+//contar hacia atras.
+
+ var cuentaAtras = [];
+
+ for (var i = 9; i > 0; i-= 2) {
+     cuentaAtras.push(i);
+ }
+
+ //console.log(cuentaAtras);
+
+ //iterar un array con un For Loop.
+
+ var myArray = [ 2, 3, 4, 5, 6];
+ var total = 0;
+
+ for (var i = 0; i < myArray.length; i++) {
+     total += myArray[i];
+ }
+
+ //console.log(total);
+
+ //nesting For Loops.
+
+ function multiplicarTodo(arr) {
+     var producto = 1;
+
+     for (var i = 0; i < arr.length; i++){
+        for (var j = 0; j < arr[i].length; j++){
+            producto *= arr[i][j];
+        }
+     }
+
+     return producto;
+ }
+
+ var producto = multiplicarTodo([[1,2],[3,4],[5,6,7]]);
+
+ //console.log(producto);
+
+ //Iterar con Do... While Loops.
+
+ var arrayTest = [];
+ var i = 10;
+
+ do {
+    arrayTest.push(i);
+    i++
+ } while (i < 5);
+
+ console.log(i , arrayTest);
---
+++ Practica JavaScript 14.js
@@ -0,0 +1,1 @@
+//Busqueda de perfil (ejercicio).
